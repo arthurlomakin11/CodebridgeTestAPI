d45f95cb2142f92824d0bd07833587a144443977
Artur <arthurlomakin11@gmail.com>
2023-06-02 16:09:21 +0300

    Added verification to check if the dog with the same name is already in the DB

--- Features/Queries/AddDogController.cs
+++ Features/Queries/AddDogController.cs
@@ -1,16 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CodebridgeTestAPI.Features;
 
-public class AddDogController
+public class AddDogController: ControllerBase
 {
     private readonly DogsDbContext _dbContext;
     public AddDogController(DogsDbContext dbContext) => _dbContext = dbContext;
     
     [HttpPost("dog")]
-    public async Task AddDog(Dog newDog)
+    public async Task<IActionResult> AddDog(Dog newDog)
     {
+        var dogExists = await _dbContext.Dogs.AnyAsync(dog => dog.Name == newDog.Name);
+        if (dogExists) return BadRequest($"Dog with name {newDog.Name} already exists");
+        
         await _dbContext.Dogs.AddAsync(newDog);
         await _dbContext.SaveChangesAsync();
+        return Ok();
     }
 }
