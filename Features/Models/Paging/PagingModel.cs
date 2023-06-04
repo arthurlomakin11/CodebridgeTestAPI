86cc2126190900ade930ccd5bc864c975979ae25
Artur <arthurlomakin11@gmail.com>
2023-06-04 13:20:07 +0300

    Added validation to paging fields

--- Features/Models/Paging/PagingModel.cs
+++ Features/Models/Paging/PagingModel.cs
@@ -1,8 +1,15 @@
-﻿namespace CodebridgeTestAPI.Features;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodebridgeTestAPI.Features;
 
 public class PagingModel: IPagingModel
 {
+    [Range(0, int.MaxValue)]
     public int pageNumber { get; set; }
+    
+    [Range(0, int.MaxValue)]
     public int pageSize { get; set; }
+    
+    [Range(0, int.MaxValue)]
     public int? limit { get; set; }
 }
--- Features/Models/PagingAndOrderingModel.cs
+++ Features/Models/PagingAndOrderingModel.cs
@@ -4,10 +4,16 @@ namespace CodebridgeTestAPI.Features;
 
 public class PagingAndOrderingModel: IPagingModel, IOrderingModel
 {
+    [Range(0, int.MaxValue)]
     public int pageNumber { get; set; }
+    
+    [Range(0, int.MaxValue)]
     public int pageSize { get; set; }
+    
+    [Range(0, int.MaxValue)]
     public int? limit { get; set; }
     public OrderType order { get; set; }
+    
     [RegularExpression("name|color|tail_length|weight", ErrorMessage = "Invalid value for attribute")]
     public string attribute { get; set; }
 }
